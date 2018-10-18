5309a2d8b473f93e9ce17d37ba5f096bf886b2cd
anhkhoa <kyoshinichiadv@gmail.com>
2018-10-19 02:29:12 +0700

    update CustomDatePickerStyle

---
+++ src/components/common-component/custom-date-picker/CustomDatePickerStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+const styles = StyleSheet.create(
+    {
+        calendarPosition: {
+            flexDirection: "row",
+            width: 300,
+        },
+        iconPos :{
+            marginTop :2
+        }
+    });
+export default styles
